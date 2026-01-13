d045ecdf2f98fc81a66725e1cb03847a9e4c438e
Quack Ranger <vaden130@gmail.com>
2026-01-13 11:56:14 +0100

    change vite, ajout server

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-
 export default defineConfig({
   plugins: [react()],
   base: "/wedding-invitation/",
+  server: {
+    port: 3000,
+    open: true,
+  },
 });
